f05907438d4837baef582104c15989e644ee52de
Rodrick <206552@qq.com>
2021-05-09 15:48:34 +0800

    5.9 finish login.html and register.html

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path, re_path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     re_path(r'index.html', views.index),
     path('404', views.error_page, name='404'),
     re_path(r'404.html', views.error_page),
+    path('login/', views.login, name='login'),
+    re_path(r'login.html', views.login),
+    path('register/', views.register, name='register'),
+    re_path(r'register.html', views.register),
 ]
--- app/views.py
+++ app/views.py
@@ -14,3 +14,13 @@ def error_page(request):
     return render(request, '404.html', context)
 
 # Create your views here.
+
+
+def login(request):
+    context = {}
+    return render(request, 'login.html', context)
+
+
+def register(request):
+    context = {}
+    return render(request, 'register.html', context)
